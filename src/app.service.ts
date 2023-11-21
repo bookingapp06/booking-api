9bbbf07384c39ed1e59e52d727d08c7af2de4e66
Andrei Sandica <andrei.sandica.94@gmail.com>
2023-11-21 17:49:10 +0200

    https message in service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! With DB';
+    return 'Hello World! With DB, http?';
   }
 }
