69f3c4bb155b9ec80568680a41c3a334cbeb2bc5
Alexandru Coarna <coarna.alexandru@gmail.com>
2023-11-22 10:04:47 +0200

    testing https again

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! With DB, http? and https?';
+    return 'Hello World, With almost complete infra!!!';
   }
 }
