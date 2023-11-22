ab14116215c75e3633a8942759bdb08ce4b3924b
Andrei Sandica <andrei.sandica.94@gmail.com>
2023-11-22 18:28:23 +0200

    no warnings in pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, With almost complete infra.final';
+    return 'Hello World, No warnings in pipeline';
   }
 }
